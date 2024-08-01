9b935d1533e4ba3da647088f15e8e94e93b1d04c
giuliapro <giulia.orsa@gmail.com>
2024-08-01 09:53:54 +0200

    first portfolio commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    // display select index
+
+    const thirdLine = document.querySelector('.third_line');
+    const selectOption = document.querySelector('.select__option');
+
+    thirdLine?.addEventListener('animationend', function () {
+        selectOption.classList.add('show-select-option');
+    });
+
+
+
+    // responsive toggle menu
+
+    const navToggle = document.querySelector('.toggle__navbar');
+    const navNavbar = document.querySelector('.navbar')
+    const navNavigation = document.querySelector('.wrap__navigation');
+    const navNavItems = document.querySelector('.wrap__nav-items')
+
+    navToggle?.addEventListener('click', function () {
+        navToggle.classList.toggle('active');
+        navNavigation.classList.toggle('show');
+        navNavItems.classList.toggle('show');
+        navNavbar.classList.toggle('active')
+    });
+
+
+    // scroll animation
+
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.1, // 10% of the element must be visible
+        rootMargin: "0px 0px -50px 0px" // Add margin at the bottom
+    };
+
+    const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+
+
+
+    // filter projects
+
+    const buttonsFilter = document.querySelectorAll('button.project__field');
+    const projects = document.querySelectorAll('.single__project');
+
+    buttonsFilter?.forEach(button => {
+        button.addEventListener('click', function () {
+            buttonsFilter.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const target = button.getAttribute('data-target');
+            projects.forEach(project => {
+                if (target === 'all' || project.getAttribute('data-target').includes(target)) {
+                    project.style.display = 'flex';
+                } else {
+                    project.style.display = 'none';
+                }
+            });
+        });
+    });
+
+
+});
