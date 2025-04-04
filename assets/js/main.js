b840439cb0b445525c14ed1577dc6718ae8e65cb
giuliapro <giulia.orsa@gmail.com>
2025-04-04 16:53:52 +0200

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,5 +95,10 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    // tooltip
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 });
